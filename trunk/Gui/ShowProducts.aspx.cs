4f19d9fc69dc5e89961505a0e239004b6dbe6a9a
silentrain.3101@gmail.com <silentrain.3101@gmail.com@1c6b3b0d-b387-7ac4-6266-ec15c60dd15d>
2011-11-18 10:11:59 +0000

    - Đã hoàn thành cơ bản phần view Phone

--- trunk/Gui/ShowProducts.aspx.cs
+++ trunk/Gui/ShowProducts.aspx.cs
@@ -13,7 +13,12 @@ public partial class Gui_ShowProducts : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            FillDataInGrid();
+            ViewState["TongTien"] = 0;
+            ViewState["TongSoLuong"] = 0;
+        }
     }
     AccessData ac = new AccessData();
     private void FillDataInGrid()
@@ -35,6 +40,23 @@ public partial class Gui_ShowProducts : System.Web.UI.Page
         finally
         {
             conn.Close();
+        }
+    }
+    protected void grid_Phone_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName=="Chon_mua")
+        {
+            //Xác định chỉ số dòng.
+            int index = int.Parse(e.CommandArgument.ToString());
+            Label donGia = (Label)grid_Phone.Rows[index].FindControl("lb_Price");
+            int temp = int.Parse(ViewState["TongSoLuong"].ToString());
+            temp += 1;
+            ViewState["TongSoLuong"] = temp.ToString();
+            temp = int.Parse(ViewState["TongTien"].ToString());
+            temp += int.Parse(donGia.Text);
+            ViewState["TongTien"] = temp.ToString();
+            lb_SoLuong.Text = ViewState["TongSoLuong"].ToString();
+            lb_TriGia.Text=ViewState["TongTien"].ToString();
         }
     }
 }
