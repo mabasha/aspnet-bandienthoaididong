afcba6b360a6f89250d0ee24c4bb1db082963e93
conghuy2012@gmail.com <conghuy2012@gmail.com@1c6b3b0d-b387-7ac4-6266-ec15c60dd15d>
2011-11-27 13:52:17 +0000

    Sorry, up thiếu cái Utils.js

---
+++ trunk/Scripts/Utils.js
@@ -0,0 +1,10 @@
+﻿function OnKeyDownCheckNumber(event) {
+
+    // Cho phép phím xóa về, và phím xóa
+    if (event.keyCode != 46 && event.keyCode != 8) {
+        //Dừng xử lý khi có phím không phải là số
+        if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+            event.preventDefault();
+        }
+    }
+}
