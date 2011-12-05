5a39d85bed6fd2943cf113a520ce3ef5d4aefae2
conghuy2012@gmail.com <conghuy2012@gmail.com@1c6b3b0d-b387-7ac4-6266-ec15c60dd15d>
2011-12-05 09:01:41 +0000

    Xong các trang Báo cáo + Jquery UI (làm chọn lịch)

--- trunk/Admin/EmployeeReport.aspx.cs
+++ trunk/Admin/EmployeeReport.aspx.cs
@@ -9,6 +9,29 @@ public partial class Admin_EmployeeReport : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            
+        }
+    }
 
+    void FillData()
+    {
+        if (tFrom.Text != "" && tTo.Text != "")
+        {
+            DateTime from = DateTime.Parse(tFrom.Text);
+            DateTime to = DateTime.Parse(tTo.Text);
+            gShow.DataSource = SaleBill.EmployeeReport(tKeyword.Text, from, to);
+        }
+        else
+        {
+            gShow.DataSource = SaleBill.EmployeeReport(tKeyword.Text);
+        }
+        
+        gShow.DataBind();
+    }
+    protected void bView_Click(object sender, EventArgs e)
+    {
+        FillData();
     }
 }
