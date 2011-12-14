9dd800ae65b0e01606074154971415c0238bde81
silentrain.3101 <silentrain.3101@gmail.com>
2011-12-14 15:43:01 +0000

    - Thêm một số file css của th

---
+++ App_Code/Models/PhoneManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PhoneManager
+/// </summary>
+public class PhoneManager
+{
+	public PhoneManager()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
